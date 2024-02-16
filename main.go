35731622b0a5445150e3b18a81af78204af2faf6
tung.tq <tung.tq@teko.vn>
2024-02-16 15:35:53 +0700

    Ziczac

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
-func isPalindrome(a []byte) bool {
-	n := len(a)
-	mid := len(a) / 2
-	for i := 0; i < mid; i++ {
-		if a[i] != a[n-i-1] {
-			return false
-		}
+import (
+	"strings"
+)
+
+func convert(s string, numRows int) string {
+	if numRows <= 1 {
+		return s
 	}
-	return true
-}
 
-func longestPalindrome(s string) string {
-	byteArr := []byte(s)
+	stepSize := (numRows - 1) + (numRows - 2) + 1
 
-	maxLen := 0
-	var result []byte
-
-	for i := range byteArr {
-		for j := i + 1; j <= len(byteArr); j++ {
-			subArr := byteArr[i:j]
-			if isPalindrome(subArr) {
-				if maxLen < len(subArr) {
-					maxLen = len(subArr)
-					result = subArr
+	var buf strings.Builder
+	for row := 0; row < numRows; row++ {
+		for startIndex := 0; startIndex < len(s)+stepSize; startIndex += stepSize {
+			if startIndex > 0 && row > 0 && row < numRows-1 {
+				leftIndex := startIndex - row
+				if leftIndex < len(s) {
+					buf.WriteByte(s[leftIndex])
 				}
+			}
+			i := startIndex + row
+			if i < len(s) {
+				buf.WriteByte(s[i])
 			}
 		}
 	}
-
-	return string(result)
+	return buf.String()
 }
--- main_test.go
+++ main_test.go
@@ -7,6 +7,19 @@ import (
 )
 
 func TestExample(t *testing.T) {
-	assert.Equal(t, "bb", longestPalindrome("cbbd"))
-	assert.Equal(t, "bab", longestPalindrome("babad"))
+	t.Run("3", func(t *testing.T) {
+		assert.Equal(t, "PAHNAPLSIIGYIR", convert("PAYPALISHIRING", 3))
+	})
+
+	t.Run("4", func(t *testing.T) {
+		assert.Equal(t, "PINALSIGYAHRPI", convert("PAYPALISHIRING", 4))
+	})
+
+	t.Run("1", func(t *testing.T) {
+		assert.Equal(t, "A", convert("A", 1))
+	})
+
+	t.Run("special", func(t *testing.T) {
+		assert.Equal(t, "ABDC", convert("ABCD", 3))
+	})
 }
