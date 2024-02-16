8b42c9e6a75f41c581ee9b739f166ba9ce4974bd
tung.tq <tung.tq@teko.vn>
2024-02-16 15:11:10 +0700

    Longest Palidrome

--- main.go
+++ main.go
@@ -1,47 +1,33 @@
 package main
 
-type bitset struct {
-	data [2]uint64
+func isPalindrome(a []byte) bool {
+	n := len(a)
+	mid := len(a) / 2
+	for i := 0; i < mid; i++ {
+		if a[i] != a[n-i-1] {
+			return false
+		}
+	}
+	return true
 }
 
-const shift = 6
-const mask = (1 << shift) - 1
-
-func (b *bitset) add(input int) {
-	x := uint64(input)
-	outer := x >> shift
-	offset := x & mask
-	oneHot := uint64(1 << offset)
-	b.data[outer] |= oneHot
-}
-
-func (b *bitset) existed(input int) bool {
-	x := uint64(input)
-	outer := x >> shift
-	offset := x & mask
-	oneHot := uint64(1 << offset)
-	return (b.data[outer] & oneHot) > 0
-}
-
-func lengthOfLongestSubstring(s string) int {
-	maxLen := 0
-
+func longestPalindrome(s string) string {
 	byteArr := []byte(s)
-	for i := range byteArr {
-		var set bitset
-		for j := i; j < len(byteArr); j++ {
-			ch := byteArr[j]
-			if set.existed(int(ch)) {
-				break
-			}
-			set.add(int(ch))
 
-			newLen := j - i + 1
-			if maxLen < newLen {
-				maxLen = newLen
+	maxLen := 0
+	var result []byte
+
+	for i := range byteArr {
+		for j := i + 1; j <= len(byteArr); j++ {
+			subArr := byteArr[i:j]
+			if isPalindrome(subArr) {
+				if maxLen < len(subArr) {
+					maxLen = len(subArr)
+					result = subArr
+				}
 			}
 		}
 	}
 
-	return maxLen
+	return string(result)
 }
--- main_test.go
+++ main_test.go
@@ -6,37 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBitSet(t *testing.T) {
-	t.Run("normal", func(t *testing.T) {
-		var b bitset
-		b.add(0)
-		assert.Equal(t, [2]uint64{1, 0}, b.data)
-
-		b.add(2)
-		assert.Equal(t, [2]uint64{0x5, 0}, b.data)
-
-		assert.Equal(t, false, b.existed(1))
-		assert.Equal(t, true, b.existed(2))
-
-		b.add(63)
-		assert.Equal(t, [2]uint64{0x8000_0000_0000_0005, 0}, b.data)
-
-		assert.Equal(t, false, b.existed(62))
-		assert.Equal(t, true, b.existed(63))
-
-		b.add(64)
-		b.add(66)
-		assert.Equal(t, [2]uint64{0x8000_0000_0000_0005, 0x5}, b.data)
-
-		assert.Equal(t, true, b.existed(66))
-		assert.Equal(t, false, b.existed(67))
-	})
-}
-
-func TestLongestSubString(t *testing.T) {
-	assert.Equal(t, 3, lengthOfLongestSubstring("abcabcbb"))
-}
-
-func TestLongestSubString_Start(t *testing.T) {
-	assert.Equal(t, 3, lengthOfLongestSubstring("abc"))
+func TestExample(t *testing.T) {
+	assert.Equal(t, "bb", longestPalindrome("cbbd"))
+	assert.Equal(t, "bab", longestPalindrome("babad"))
 }
