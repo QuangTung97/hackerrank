8e9944483fec5ce7ba9d9f15e439018ac970e8f9
tung.tq <tung.tq@teko.vn>
2024-02-17 13:26:29 +0700

    Median Sorted

--- main.go
+++ main.go
@@ -1,12 +1,100 @@
 package main
 
-func removeDuplicates(nums []int) int {
-	nextIndex := 0
-	for i := 0; i < len(nums); i++ {
-		if i == 0 || nums[i] > nums[i-1] {
-			nums[nextIndex] = nums[i]
-			nextIndex++
-		}
+import (
+	"sort"
+)
+
+func medianOf(nums []int) float64 {
+	n := len(nums)
+	index := n / 2
+	if n%2 == 0 {
+		return float64(nums[index]+nums[index-1]) / 2.0
 	}
-	return nextIndex
+	return float64(nums[index])
+}
+
+func simpleForm(nums, simpler []int) float64 {
+	if len(simpler) == 0 {
+		return medianOf(nums)
+	}
+
+	x := simpler[0]
+
+	if len(nums) == 0 {
+		return float64(x)
+	}
+
+	n := len(nums)
+	index := n / 2
+	if n%2 == 0 {
+		v1 := nums[index-1]
+		v2 := nums[index]
+		if x < v1 {
+			return float64(v1)
+		}
+		if x > v2 {
+			return float64(v2)
+		}
+		return float64(x)
+	} else {
+		v := nums[index]
+		if len(nums) == 1 {
+			return float64(v+x) / 2.0
+		}
+
+		v1 := nums[index-1]
+		v2 := nums[index+1]
+
+		if x < v1 {
+			return float64(v1+v) / 2.0
+		}
+		if x > v2 {
+			return float64(v+v2) / 2.0
+		}
+
+		return float64(v+x) / 2.0
+	}
+}
+
+func form2Solution(nums1 []int, nums2 []int) float64 {
+	var x []int
+	x = append(x, nums1...)
+	x = append(x, nums2...)
+	sort.Ints(x)
+	return medianOf(x)
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1) <= 1 {
+		return simpleForm(nums2, nums1)
+	}
+	if len(nums2) <= 1 {
+		return simpleForm(nums1, nums2)
+	}
+
+	if len(nums1) == 2 {
+		return form2Solution(nums2, nums1)
+	}
+	if len(nums2) == 2 {
+		return form2Solution(nums1, nums2)
+	}
+
+	lowerLen := func(x []int) int {
+		return (len(x) - 1) / 2
+	}
+
+	smallerLen := min(lowerLen(nums1), lowerLen(nums2))
+	m1 := medianOf(nums1)
+	m2 := medianOf(nums2)
+
+	if m1 < m2 {
+		return findMedianSortedArrays(
+			nums1[smallerLen:],
+			nums2[:len(nums2)-smallerLen],
+		)
+	}
+	return findMedianSortedArrays(
+		nums1[:len(nums1)-smallerLen],
+		nums2[smallerLen:],
+	)
 }
--- main_test.go
+++ main_test.go
@@ -6,8 +6,83 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMeanOf(t *testing.T) {
+	assert.Equal(t, 2.0, medianOf([]int{1, 2, 3}))
+	assert.Equal(t, 2.5, medianOf([]int{1, 2, 3, 4}))
+	assert.Equal(t, 1.0, medianOf([]int{1}))
+	assert.Equal(t, 4.5, medianOf([]int{4, 5}))
+}
+
 func TestExample(t *testing.T) {
-	arr := []int{1, 1, 2, 2, 2, 3, 4, 5, 5, 6}
-	index := removeDuplicates(arr)
-	assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, arr[:index])
+	t.Run("case1", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{1, 3},
+			[]int{2},
+		)
+		assert.Equal(t, 2.0, v)
+	})
+
+	t.Run("case2", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{1, 2},
+			[]int{3, 4},
+		)
+		assert.Equal(t, 2.5, v)
+	})
+
+	t.Run("case3", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{2},
+			[]int{},
+		)
+		assert.Equal(t, 2.0, v)
+	})
+
+	t.Run("case4", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{3},
+			[]int{-2, -1},
+		)
+		assert.Equal(t, -1.0, v)
+	})
+
+	t.Run("case5", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{2, 2, 4, 4},
+			[]int{2, 2, 4, 4},
+		)
+		assert.Equal(t, 3.0, v)
+	})
+
+	t.Run("case6", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{1, 2},
+			[]int{-1, 3},
+		)
+		assert.Equal(t, 1.5, v)
+	})
+
+	t.Run("case7", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{1},
+			[]int{2, 3, 4},
+		)
+		assert.Equal(t, 2.5, v)
+	})
+
+	t.Run("case8", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{1, 2, 6, 7},
+			[]int{3, 4, 5, 8},
+		)
+		assert.Equal(t, 4.5, v)
+	})
+
+	t.Run("case9", func(t *testing.T) {
+		v := findMedianSortedArrays(
+			[]int{0, 0, 0, 0, 0},
+			[]int{-1, 0, 0, 0, 0, 0, 1},
+		)
+		assert.Equal(t, 0.0, v)
+	})
 }
