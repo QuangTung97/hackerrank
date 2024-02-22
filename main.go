67cef350cd91ffa60768b4b12570a3dd3840e7a8
tung.tq <tung.tq@teko.vn>
2024-02-22 18:33:40 +0700

    3Sum first solution

--- main.go
+++ main.go
@@ -1,42 +1,61 @@
 package main
 
 import (
-	"strings"
+	"sort"
 )
 
-type symbol struct {
-	str string
-	val int
+func lowerBound(nums []int, x int) int {
+	first := 0
+	last := len(nums)
+	for first < last {
+		mid := first + (last-first)/2
+		if nums[mid] < x {
+			first = mid + 1
+		} else {
+			last = mid
+		}
+	}
+	return first
 }
 
-var symbols = []symbol{
-	{str: "I", val: 1},
-	{str: "IV", val: 4},
-	{str: "V", val: 5},
-	{str: "IX", val: 9},
-	{str: "X", val: 10},
-	{str: "XL", val: 40},
-	{str: "L", val: 50},
-	{str: "XC", val: 90},
-	{str: "C", val: 100},
-	{str: "CD", val: 400},
-	{str: "D", val: 500},
-	{str: "CM", val: 900},
-	{str: "M", val: 1000},
+type triplet struct {
+	a int
+	b int
+	c int
 }
 
-func intToRoman(num int) string {
-	var b strings.Builder
-Outer:
-	for num > 0 {
-		for i := len(symbols) - 1; i >= 0; i-- {
-			s := symbols[i]
-			if num >= s.val {
-				num -= s.val
-				b.WriteString(s.str)
-				continue Outer
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+
+	existed := map[triplet]struct{}{}
+	var result [][]int
+
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			twoSum := nums[i] + nums[j]
+
+			searchNums := nums[j+1:]
+			for {
+				index := lowerBound(searchNums, -twoSum)
+				index2 := lowerBound(searchNums, -twoSum+1)
+				if index >= len(searchNums) || searchNums[index] != -twoSum {
+					break
+				}
+
+				newTuple := []int{nums[i], nums[j], searchNums[index]}
+				t := triplet{
+					a: newTuple[0],
+					b: newTuple[1],
+					c: newTuple[2],
+				}
+				_, ok := existed[t]
+				if !ok {
+					existed[t] = struct{}{}
+					result = append(result, newTuple)
+				}
+				searchNums = searchNums[index2:]
 			}
 		}
 	}
-	return b.String()
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -6,10 +6,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRoman(t *testing.T) {
+func TestBinarySearch(t *testing.T) {
+	assert.Equal(t, 1, lowerBound([]int{1, 2, 3, 5, 6, 6, 7}, 2))
+	assert.Equal(t, 6, lowerBound([]int{1, 2, 3, 5, 6, 6, 7}, 7))
+	assert.Equal(t, 4, lowerBound([]int{1, 2, 3, 5, 6, 6, 7}, 6))
+	assert.Equal(t, 0, lowerBound([]int{1, 1, 1}, 1))
+	assert.Equal(t, 3, lowerBound([]int{1, 1, 1}, 3))
+}
+func Test3Sum(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
-		assert.Equal(t, "III", intToRoman(3))
-		assert.Equal(t, "LVIII", intToRoman(58))
-		assert.Equal(t, "MCMXCIV", intToRoman(1994))
+		assert.Equal(t, [][]int{
+			{-1, -1, 2},
+			{-1, 0, 1},
+		}, threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		assert.Equal(t, [][]int{
+			{0, 0, 0},
+		}, threeSum([]int{0, 0, 0}))
+	})
+
+	t.Run("duplicated", func(t *testing.T) {
+		assert.Equal(t, [][]int{
+			{-4, -2, 6}, {-4, 0, 4}, {-4, 1, 3}, {-4, 2, 2}, {-2, -2, 4}, {-2, 0, 2},
+		}, threeSum([]int{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}))
 	})
 }
