fa87da136bea7d0890281069cab0eac6ad6bfd35
tung.tq <tung.tq@teko.vn>
2024-02-23 09:31:41 +0700

    31. Next Permutation Medium

--- main.go
+++ main.go
@@ -1,64 +1,31 @@
 package main
 
-func findRotatePoint(nums []int) int {
-	first := 0
-	last := len(nums) - 1
-	for first < last-1 {
-		mid := first + (last-first)/2
-		if nums[first] > nums[mid] {
-			last = mid
-		} else {
-			first = mid
+import (
+	"slices"
+)
+
+func nextPermutation(nums []int) {
+	mid := -1
+	for i := len(nums) - 1; i >= 1; i-- {
+		if nums[i] > nums[i-1] {
+			mid = i
+			break
 		}
 	}
-	if nums[first] > nums[last] {
-		return last
-	}
-	return first
-}
 
-func lowerBound(nums []int, a int) int {
-	first := 0
-	last := len(nums)
-	for first < last {
-		mid := first + (last-first)/2
-		if a <= nums[mid] {
-			last = mid
-		} else {
-			first = mid + 1
+	if mid < 0 {
+		slices.Reverse(nums)
+		return
+	}
+
+	right := mid
+	for i := mid + 1; i < len(nums); i++ {
+		if nums[i] <= nums[mid-1] {
+			break
 		}
+		right = i
 	}
-	return first
-}
+	nums[mid-1], nums[right] = nums[right], nums[mid-1]
 
-func upperBound(nums []int, x int) int {
-	first := 0
-	last := len(nums)
-	for first < last {
-		mid := first + (last-first)/2
-		if x < nums[mid] {
-			last = mid
-		} else {
-			first = mid + 1
-		}
-	}
-	return first
-}
-
-func search(nums []int, target int) int {
-	rotate := findRotatePoint(nums)
-
-	a := nums[:rotate]
-	index := lowerBound(a, target)
-	if index < len(a) && a[index] == target {
-		return index
-	}
-
-	b := nums[rotate:]
-	index = lowerBound(b, target)
-	if index < len(b) && b[index] == target {
-		return rotate + index
-	}
-
-	return -1
+	slices.Reverse(nums[mid:])
 }
--- main_test.go
+++ main_test.go
@@ -6,41 +6,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindRotatePoint(t *testing.T) {
-	t.Run("normal", func(t *testing.T) {
-		assert.Equal(t, 4, findRotatePoint([]int{4, 5, 6, 7, 0, 1, 2}))
-		assert.Equal(t, 3, findRotatePoint([]int{4, 5, 6, 0, 1, 2}))
-		assert.Equal(t, 1, findRotatePoint([]int{6, 0, 1, 2}))
-	})
-	t.Run("special", func(t *testing.T) {
-		assert.Equal(t, 2, findRotatePoint([]int{9, 10, 0, 1, 2}))
-	})
-}
-
-func TestLowerBound(t *testing.T) {
-	assert.Equal(t, 2, lowerBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 3))
-	assert.Equal(t, 4, upperBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 3))
-
-	assert.Equal(t, 6, lowerBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 6))
-	assert.Equal(t, 8, upperBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 6))
-
-	assert.Equal(t, 5, lowerBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 5))
-	assert.Equal(t, 6, upperBound([]int{1, 2, 3, 3, 4, 5, 6, 6, 7}, 5))
-
-	assert.Equal(t, 5, lowerBound([]int{1, 2, 3, 3, 4}, 6))
-	assert.Equal(t, 5, upperBound([]int{1, 2, 3, 3, 4}, 6))
-
-	assert.Equal(t, 2, lowerBound([]int{1, 2}, 6))
-	assert.Equal(t, 2, upperBound([]int{1, 2}, 6))
-
-	assert.Equal(t, 0, lowerBound([]int{1, 2}, 1))
-	assert.Equal(t, 1, upperBound([]int{1, 2}, 1))
-
-	assert.Equal(t, 1, lowerBound([]int{1, 2}, 2))
-	assert.Equal(t, 2, upperBound([]int{1, 2}, 2))
-}
-
 func TestFind(t *testing.T) {
-	assert.Equal(t, 4, search([]int{4, 5, 6, 7, 0, 1, 2}, 0))
-	assert.Equal(t, -1, search([]int{4, 5, 6, 7, 0, 1, 2}, 3))
+	nums := []int{1, 2, 3}
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{1, 3, 2}, nums)
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{2, 1, 3}, nums)
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{2, 3, 1}, nums)
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{3, 1, 2}, nums)
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{3, 2, 1}, nums)
+
+	nextPermutation(nums)
+	assert.Equal(t, []int{1, 2, 3}, nums)
+}
+
+func TestFind_Special(t *testing.T) {
+	nums := []int{1, 5, 1}
+	nextPermutation(nums)
+	assert.Equal(t, []int{5, 1, 1}, nums)
 }
