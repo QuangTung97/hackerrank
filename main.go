f0e7eb4b19f2f68053efd7d8e7e2904090998c61
tung.tq <tung.tq@teko.vn>
2024-02-23 11:50:35 +0700

    61. Rotate List Medium

--- main.go
+++ main.go
@@ -1,38 +1,54 @@
 package main
 
-import (
-	"sort"
-)
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-func threeSum(nums []int) [][]int {
-	var result [][]int
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-2; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
-		}
-
-		l := i + 1
-		r := len(nums) - 1
-		for l < r {
-			sum := nums[i] + nums[l] + nums[r]
-			if sum < 0 {
-				l++
-				continue
-			}
-			if sum > 0 {
-				r--
-				continue
-			}
-			result = append(result, []int{nums[i], nums[l], nums[r]})
-			l++
-			for l < r {
-				if nums[l] != nums[l-1] {
-					break
-				}
-				l++
-			}
+func newList(nums ...int) *ListNode {
+	var h *ListNode
+	for i := len(nums) - 1; i >= 0; i-- {
+		h = &ListNode{
+			Next: h,
+			Val:  nums[i],
 		}
 	}
+	return h
+}
+
+func toInts(h *ListNode) []int {
+	var result []int
+	for ; h != nil; h = h.Next {
+		result = append(result, h.Val)
+	}
+	return result
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	count := 0
+	var last *ListNode
+	for h := head; h != nil; h = h.Next {
+		count++
+		last = h
+	}
+	if count == 0 {
+		return head
+	}
+
+	shift := k % count
+	if shift == 0 {
+		return head
+	}
+
+	result := head
+	ptr := &result
+	for i := 0; i < count-shift; i++ {
+		ptr = &(*ptr).Next
+	}
+
+	last.Next = head
+	result = *ptr
+	*ptr = nil
+
 	return result
 }
--- main_test.go
+++ main_test.go
@@ -6,9 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFind(t *testing.T) {
-	assert.Equal(t, [][]int{
-		{-1, -1, 2},
-		{-1, 0, 1},
-	}, threeSum([]int{-1, 0, 1, 2, -1, -4}))
+func TestToList(t *testing.T) {
+	h := newList(1, 2, 3, 4)
+	assert.Equal(t, []int{1, 2, 3, 4}, toInts(h))
+}
+
+func TestRotate(t *testing.T) {
+	h := newList(1, 2, 3, 4, 5)
+	newL := rotateRight(h, 2)
+	assert.Equal(t, []int{4, 5, 1, 2, 3}, toInts(newL))
+}
+
+func TestRotate_Case2(t *testing.T) {
+	h := newList(1)
+	newL := rotateRight(h, 0)
+	assert.Equal(t, []int{1}, toInts(newL))
 }
