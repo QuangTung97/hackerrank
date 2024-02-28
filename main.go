3d7bea503b1c6dbd3a134c2f7249471c7d8134e1
tung.tq <tung.tq@teko.vn>
2024-02-28 10:59:05 +0700

    43. Multiply Strings Medium

--- main.go
+++ main.go
@@ -1,31 +1,50 @@
 package main
 
 import (
-	"slices"
+	"strings"
 )
 
-func intersect(a, b []int) bool {
-	if b[0] <= a[1] {
-		return true
+func reverse(s string) string {
+	var buf strings.Builder
+	for i := len(s) - 1; i >= 0; i-- {
+		buf.WriteByte(s[i])
 	}
-	return false
+	return buf.String()
 }
 
-func merge(intervals [][]int) [][]int {
-	slices.SortFunc(intervals, func(a, b []int) int {
-		return a[0] - b[0]
-	})
+func multiply(num1 string, num2 string) string {
+	num1 = reverse(num1)
+	num2 = reverse(num2)
 
-	var result [][]int
-	result = append(result, intervals[0])
+	result := make([]byte, 10000)
+	last := 0
 
-	for i := 1; i < len(intervals); i++ {
-		last := result[len(result)-1]
-		if intersect(last, intervals[i]) {
-			last[1] = max(last[1], intervals[i][1])
-		} else {
-			result = append(result, intervals[i])
+	rem := uint8(0)
+	for i := 0; i < len(num2); i++ {
+		b := num2[i] - '0'
+		for j := 0; j < len(num1); j++ {
+			a := num1[j] - '0'
+			m := a*b + rem + result[i+j]
+			rem = m / 10
+			result[i+j] = m % 10
+			if m > 0 {
+				last = max(last, i+j+1)
+			}
+		}
+		if rem > 0 {
+			result[last] = rem
+			rem = 0
+			last++
 		}
 	}
-	return result
+
+	for i := range result[:last] {
+		result[i] = result[i] + '0'
+	}
+
+	if last == 0 {
+		return "0"
+	}
+
+	return reverse(string(result[:last]))
 }
--- main_test.go
+++ main_test.go
@@ -6,15 +6,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMerge(t *testing.T) {
-	assert.Equal(t, [][]int{
-		{1, 6},
-		{8, 10},
-		{15, 18},
-	}, merge([][]int{
-		{1, 3},
-		{2, 6},
-		{8, 10},
-		{15, 18},
-	}))
+func TestMul(t *testing.T) {
+	assert.Equal(t, "48", multiply("12", "4"))
+}
+
+func TestMul_Case2(t *testing.T) {
+	assert.Equal(t, "52", multiply("13", "4"))
+}
+
+func TestMul_Case3(t *testing.T) {
+	assert.Equal(t, "56088", multiply("123", "456"))
+}
+
+func TestMul_Case4(t *testing.T) {
+	assert.Equal(t, "81", multiply("9", "9"))
+}
+
+func TestMul_Case5(t *testing.T) {
+	assert.Equal(t, "998001", multiply("999", "999"))
+}
+
+func TestMul_Case6(t *testing.T) {
+	assert.Equal(t, "9801", multiply("99", "99"))
+}
+
+func TestMul_Case7(t *testing.T) {
+	assert.Equal(t, "0", multiply("9133", "0"))
 }
