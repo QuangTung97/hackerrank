e86bc72b91bd8bc5e8e0c8f11f88fc0557f68eb7
tung.tq <tung.tq@teko.vn>
2024-02-28 14:44:37 +0700

    Two Sum Input Is Sorted Medium

--- main.go
+++ main.go
@@ -1,50 +1,19 @@
 package main
 
-import (
-	"strings"
-)
-
-func reverse(s string) string {
-	var buf strings.Builder
-	for i := len(s) - 1; i >= 0; i-- {
-		buf.WriteByte(s[i])
-	}
-	return buf.String()
-}
-
-func multiply(num1 string, num2 string) string {
-	num1 = reverse(num1)
-	num2 = reverse(num2)
-
-	result := make([]byte, 10000)
-	last := 0
-
-	rem := uint8(0)
-	for i := 0; i < len(num2); i++ {
-		b := num2[i] - '0'
-		for j := 0; j < len(num1); j++ {
-			a := num1[j] - '0'
-			m := a*b + rem + result[i+j]
-			rem = m / 10
-			result[i+j] = m % 10
-			if m > 0 {
-				last = max(last, i+j+1)
-			}
+func twoSum(numbers []int, target int) []int {
+	l := 0
+	r := len(numbers) - 1
+	for l < r {
+		sum := numbers[l] + numbers[r] - target
+		if sum < 0 {
+			l++
+			continue
 		}
-		if rem > 0 {
-			result[last] = rem
-			rem = 0
-			last++
+		if sum > 0 {
+			r--
+			continue
 		}
+		return []int{l + 1, r + 1}
 	}
-
-	for i := range result[:last] {
-		result[i] = result[i] + '0'
-	}
-
-	if last == 0 {
-		return "0"
-	}
-
-	return reverse(string(result[:last]))
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -6,30 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMul(t *testing.T) {
-	assert.Equal(t, "48", multiply("12", "4"))
-}
-
-func TestMul_Case2(t *testing.T) {
-	assert.Equal(t, "52", multiply("13", "4"))
-}
-
-func TestMul_Case3(t *testing.T) {
-	assert.Equal(t, "56088", multiply("123", "456"))
-}
-
-func TestMul_Case4(t *testing.T) {
-	assert.Equal(t, "81", multiply("9", "9"))
-}
-
-func TestMul_Case5(t *testing.T) {
-	assert.Equal(t, "998001", multiply("999", "999"))
-}
-
-func TestMul_Case6(t *testing.T) {
-	assert.Equal(t, "9801", multiply("99", "99"))
-}
-
-func TestMul_Case7(t *testing.T) {
-	assert.Equal(t, "0", multiply("9133", "0"))
+func TestTwoSum(t *testing.T) {
+	assert.Equal(t, []int{1, 2}, twoSum([]int{2, 7, 11, 15}, 9))
 }
