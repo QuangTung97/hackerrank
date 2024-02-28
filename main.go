f4149db626f7d1578173468431a46ad6c77b65aa
tung.tq <tung.tq@teko.vn>
2024-02-28 15:02:41 +0700

    Factorial Trailing Zeros Medium

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-func twoSum(numbers []int, target int) []int {
-	l := 0
-	r := len(numbers) - 1
-	for l < r {
-		sum := numbers[l] + numbers[r] - target
-		if sum < 0 {
-			l++
-			continue
-		}
-		if sum > 0 {
-			r--
-			continue
-		}
-		return []int{l + 1, r + 1}
+func countNumberOf(n int, x int) int {
+	count := 0
+	base := x
+	for base <= n {
+		count += n / base
+		base *= x
 	}
-	return nil
+	return count
+}
+
+func trailingZeroes(n int) int {
+	return min(countNumberOf(n, 2), countNumberOf(n, 5))
 }
--- main_test.go
+++ main_test.go
@@ -6,6 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTwoSum(t *testing.T) {
-	assert.Equal(t, []int{1, 2}, twoSum([]int{2, 7, 11, 15}, 9))
+func TestCount(t *testing.T) {
+	assert.Equal(t, 0, countNumberOf(1, 2))
+	assert.Equal(t, 8, countNumberOf(10, 2))
+	assert.Equal(t, 97, countNumberOf(100, 2))
+	assert.Equal(t, 25, countNumberOf(28, 2))
+	assert.Equal(t, 127, countNumberOf(128, 2))
+}
+
+func TestTrailingZero(t *testing.T) {
+	assert.Equal(t, 0, trailingZeroes(3))
+	assert.Equal(t, 1, trailingZeroes(5))
+	assert.Equal(t, 7, trailingZeroes(30))
+}
+
+func TestTrailingZero_1(t *testing.T) {
+	assert.Equal(t, 7, trailingZeroes(30))
 }
