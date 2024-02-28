7f2f87002bad80ed32c1a0d421e5a0bbfaf97b0b
tung.tq <tung.tq@teko.vn>
2024-02-28 10:16:28 +0700

    38. Count And Say Medium

--- main.go
+++ main.go
@@ -1,48 +1,36 @@
 package main
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+import (
+	"strconv"
+	"strings"
+)
 
-func toInts(l *ListNode) []int {
-	var result []int
-	for ; l != nil; l = l.Next {
-		result = append(result, l.Val)
-	}
-	return result
-}
-
-func newList(x ...int) *ListNode {
-	var l *ListNode
-	for i := len(x) - 1; i >= 0; i-- {
-		l = &ListNode{
-			Val:  x[i],
-			Next: l,
-		}
-	}
-	return l
-}
-
-func swapPairs(head *ListNode) *ListNode {
-	pfirst := &head
-	for {
-		first := *pfirst
-		if first == nil {
-			break
-		}
-		second := first.Next
-		if second == nil {
-			break
-		}
-
-		*pfirst = second
-
-		first.Next = second.Next
-		second.Next = first
-
-		pfirst = &first.Next
+func countAndSay(n int) string {
+	if n == 1 {
+		return "1"
 	}
 
-	return head
+	last := countAndSay(n - 1)
+	var res strings.Builder
+
+	prevCh := last[0]
+	count := 1
+	for i := 1; i < len(last); i++ {
+		ch := last[i]
+		if ch == prevCh {
+			count++
+			continue
+		}
+
+		res.WriteString(strconv.FormatInt(int64(count), 10))
+		res.WriteByte(prevCh)
+
+		count = 1
+		prevCh = ch
+	}
+
+	res.WriteString(strconv.FormatInt(int64(count), 10))
+	res.WriteByte(prevCh)
+
+	return res.String()
 }
--- main_test.go
+++ main_test.go
@@ -6,17 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
-	l := newList(1, 2, 3, 4)
-	assert.Equal(t, []int{1, 2, 3, 4}, toInts(l))
-}
-
-func TestSwap(t *testing.T) {
-	l := swapPairs(newList(1, 2, 3, 4))
-	assert.Equal(t, []int{2, 1, 4, 3}, toInts(l))
-}
-
-func TestSwap_2(t *testing.T) {
-	l := swapPairs(newList(1, 2, 3, 4, 5))
-	assert.Equal(t, []int{2, 1, 4, 3, 5}, toInts(l))
+func TestCountAndSay(t *testing.T) {
+	assert.Equal(t, "1", countAndSay(1))
+	assert.Equal(t, "1211", countAndSay(4))
 }
