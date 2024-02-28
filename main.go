837934813dc50b376f4c47d48c230f8e0fceef01
tung.tq <tung.tq@teko.vn>
2024-02-28 15:54:06 +0700

    Binary Search Tree Iterator

--- main.go
+++ main.go
@@ -1,15 +1,66 @@
 package main
 
-func countNumberOf(n int, x int) int {
-	count := 0
-	base := x
-	for base <= n {
-		count += n / base
-		base *= x
-	}
-	return count
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-func trailingZeroes(n int) int {
-	return min(countNumberOf(n, 2), countNumberOf(n, 5))
+type BSTIterator struct {
+	path []*TreeNode
+}
+
+func getAllLeftNodes(n *TreeNode) []*TreeNode {
+	var path []*TreeNode
+	for ; n != nil; n = n.Left {
+		path = append(path, n)
+	}
+	return path
+}
+
+func pathValues(p []*TreeNode) []int {
+	var r []int
+	for _, n := range p {
+		r = append(r, n.Val)
+	}
+	return r
+}
+
+func computeNextPath(p []*TreeNode) []*TreeNode {
+	var child *TreeNode
+	var back bool
+	for {
+		n := len(p)
+		if n == 0 {
+			return nil
+		}
+		last := p[n-1]
+		if last.Right == child {
+			p = p[:n-1]
+			child = last
+			back = true
+			continue
+		}
+		if back {
+			return p
+		}
+		return append(p, getAllLeftNodes(last.Right)...)
+	}
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+	return BSTIterator{
+		path: getAllLeftNodes(root),
+	}
+}
+
+func (i *BSTIterator) Next() int {
+	last := i.path[len(i.path)-1]
+	newP := computeNextPath(i.path)
+	i.path = newP
+	return last.Val
+}
+
+func (i *BSTIterator) HasNext() bool {
+	return len(i.path) > 0
 }
--- main_test.go
+++ main_test.go
@@ -6,20 +6,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func drain(it *BSTIterator) []int {
+	var res []int
+	for {
+		res = append(res, it.Next())
+		if !it.HasNext() {
+			return res
+		}
+	}
+}
+
 func TestCount(t *testing.T) {
-	assert.Equal(t, 0, countNumberOf(1, 2))
-	assert.Equal(t, 8, countNumberOf(10, 2))
-	assert.Equal(t, 97, countNumberOf(100, 2))
-	assert.Equal(t, 25, countNumberOf(28, 2))
-	assert.Equal(t, 127, countNumberOf(128, 2))
-}
+	n1 := &TreeNode{Val: 3}
+	n2 := &TreeNode{Val: 7}
+	n3 := &TreeNode{Val: 9}
+	n4 := &TreeNode{Val: 15}
+	n5 := &TreeNode{Val: 20}
 
-func TestTrailingZero(t *testing.T) {
-	assert.Equal(t, 0, trailingZeroes(3))
-	assert.Equal(t, 1, trailingZeroes(5))
-	assert.Equal(t, 7, trailingZeroes(30))
-}
+	n2.Left = n1
+	n2.Right = n4
+	n4.Left = n3
+	n4.Right = n5
 
-func TestTrailingZero_1(t *testing.T) {
-	assert.Equal(t, 7, trailingZeroes(30))
+	p := getAllLeftNodes(n2)
+	assert.Equal(t, []int{7, 3}, pathValues(p))
+	assert.Equal(t, []int{15, 9}, pathValues(getAllLeftNodes(n2.Right)))
+
+	// check next path
+	p = getAllLeftNodes(n2)
+
+	p1 := computeNextPath(p)
+	assert.Equal(t, []int{7}, pathValues(p1))
+
+	p2 := computeNextPath(p1)
+	assert.Equal(t, []int{7, 15, 9}, pathValues(p2))
+
+	p3 := computeNextPath(p2)
+	assert.Equal(t, []int{7, 15}, pathValues(p3))
+
+	p4 := computeNextPath(p3)
+	assert.Equal(t, []int{7, 15, 20}, pathValues(p4))
+
+	p5 := computeNextPath(p4)
+	assert.Equal(t, []int(nil), pathValues(p5))
+
+	// check drain it
+	it := Constructor(n2)
+	nodes := drain(&it)
+	assert.Equal(t, []int{3, 7, 9, 15, 20}, nodes)
 }
