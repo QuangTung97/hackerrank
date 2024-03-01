6a394a4fa403b1aa7c36766ef0d9a077e19f49dc
tung.tq <tung.tq@teko.vn>
2024-03-01 09:55:19 +0700

    Power(x, n) Medium

--- main.go
+++ main.go
@@ -1,66 +1,15 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-type BSTIterator struct {
-	path []*TreeNode
-}
-
-func getAllLeftNodes(n *TreeNode) []*TreeNode {
-	var path []*TreeNode
-	for ; n != nil; n = n.Left {
-		path = append(path, n)
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		return 1.0 / myPow(x, -n)
 	}
-	return path
-}
-
-func pathValues(p []*TreeNode) []int {
-	var r []int
-	for _, n := range p {
-		r = append(r, n.Val)
+	if n == 0 {
+		return 1.0
 	}
-	return r
-}
-
-func computeNextPath(p []*TreeNode) []*TreeNode {
-	var child *TreeNode
-	var back bool
-	for {
-		n := len(p)
-		if n == 0 {
-			return nil
-		}
-		last := p[n-1]
-		if last.Right == child {
-			p = p[:n-1]
-			child = last
-			back = true
-			continue
-		}
-		if back {
-			return p
-		}
-		return append(p, getAllLeftNodes(last.Right)...)
+	v := myPow(x, n>>1)
+	if n&0x1 == 0 {
+		return v * v
 	}
-}
-
-func Constructor(root *TreeNode) BSTIterator {
-	return BSTIterator{
-		path: getAllLeftNodes(root),
-	}
-}
-
-func (i *BSTIterator) Next() int {
-	last := i.path[len(i.path)-1]
-	newP := computeNextPath(i.path)
-	i.path = newP
-	return last.Val
-}
-
-func (i *BSTIterator) HasNext() bool {
-	return len(i.path) > 0
+	return v * v * x
 }
--- main_test.go
+++ main_test.go
@@ -6,52 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func drain(it *BSTIterator) []int {
-	var res []int
-	for {
-		res = append(res, it.Next())
-		if !it.HasNext() {
-			return res
-		}
-	}
-}
-
-func TestCount(t *testing.T) {
-	n1 := &TreeNode{Val: 3}
-	n2 := &TreeNode{Val: 7}
-	n3 := &TreeNode{Val: 9}
-	n4 := &TreeNode{Val: 15}
-	n5 := &TreeNode{Val: 20}
-
-	n2.Left = n1
-	n2.Right = n4
-	n4.Left = n3
-	n4.Right = n5
-
-	p := getAllLeftNodes(n2)
-	assert.Equal(t, []int{7, 3}, pathValues(p))
-	assert.Equal(t, []int{15, 9}, pathValues(getAllLeftNodes(n2.Right)))
-
-	// check next path
-	p = getAllLeftNodes(n2)
-
-	p1 := computeNextPath(p)
-	assert.Equal(t, []int{7}, pathValues(p1))
-
-	p2 := computeNextPath(p1)
-	assert.Equal(t, []int{7, 15, 9}, pathValues(p2))
-
-	p3 := computeNextPath(p2)
-	assert.Equal(t, []int{7, 15}, pathValues(p3))
-
-	p4 := computeNextPath(p3)
-	assert.Equal(t, []int{7, 15, 20}, pathValues(p4))
-
-	p5 := computeNextPath(p4)
-	assert.Equal(t, []int(nil), pathValues(p5))
-
-	// check drain it
-	it := Constructor(n2)
-	nodes := drain(&it)
-	assert.Equal(t, []int{3, 7, 9, 15, 20}, nodes)
+func TestPower(t *testing.T) {
+	assert.Equal(t, 4.0, myPow(2.0, 2))
+	assert.Equal(t, 9.0, myPow(3.0, 2))
+	assert.Equal(t, 27.0, myPow(3.0, 3))
+	assert.Equal(t, 1.0/27.0, myPow(3.0, -3))
 }
