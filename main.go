ad657a480a45e0c6eb85e2437572332c23eb45c1
tung.tq <tung.tq@teko.vn>
2024-03-05 08:48:34 +0700

    29. Divide Two Integers - Medium

--- main.go
+++ main.go
@@ -1,15 +1,60 @@
 package main
 
-func removeElement(nums []int, val int) int {
-	last := len(nums)
-	for i := 0; i < last; {
-		if nums[i] != val {
-			i++
+import (
+	"math"
+)
+
+func bitLen(a int) int {
+	if a == 0 {
+		return 1
+	}
+
+	n := 0
+	for a > 0 {
+		n++
+		a = a >> 1
+	}
+	return n
+}
+
+func divide(dividend int, divisor int) int {
+	sign := 1
+	if dividend < 0 {
+		sign = -sign
+		dividend = -dividend
+	}
+	if divisor < 0 {
+		sign = -sign
+		divisor = -divisor
+	}
+
+	aLen := bitLen(dividend)
+	bLen := bitLen(divisor)
+	if aLen < bLen {
+		return 0
+	}
+
+	remain := aLen - bLen
+	result := 0
+	for shift := remain; shift >= 0; shift-- {
+		result <<= 1
+
+		v := divisor << shift
+		if v > dividend {
 			continue
 		}
-
-		last--
-		nums[i] = nums[last]
+		dividend -= v
+		result |= 1
 	}
-	return last
+	if sign < 0 {
+		result = -result
+	}
+
+	if result > math.MaxInt32 {
+		return math.MaxInt32
+	}
+	if result < math.MinInt32 {
+		return math.MinInt32
+	}
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -6,8 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestBitLen(t *testing.T) {
+	assert.Equal(t, 1, bitLen(0))
+	assert.Equal(t, 1, bitLen(1))
+	assert.Equal(t, 2, bitLen(2))
+	assert.Equal(t, 4, bitLen(15))
+	assert.Equal(t, 5, bitLen(16))
+}
+
 func TestRemove(t *testing.T) {
-	a := []int{3, 2, 2, 3}
-	index := removeElement(a, 3)
-	assert.Equal(t, []int{2, 2}, a[:index])
+	assert.Equal(t, 3, divide(10, 3))
+	assert.Equal(t, -2, divide(7, -3))
+	assert.Equal(t, 3, divide(25, 8))
+	assert.Equal(t, 4, divide(32, 8))
+	assert.Equal(t, 3, divide(31, 8))
+	assert.Equal(t, 10, divide(31, 3))
+	assert.Equal(t, 10, divide(32, 3))
+	assert.Equal(t, 11, divide(33, 3))
+	assert.Equal(t, -10, divide(-32, 3))
+	assert.Equal(t, -11, divide(-33, 3))
+	assert.Equal(t, 2147483647, divide(-2147483648, -1))
 }
