8b961aae5f1512d44087e1ec77b6f7da394bea52
tung.tq <tung.tq@teko.vn>
2024-03-05 08:33:44 +0700

    27. Remove Element - Medium

--- main.go
+++ main.go
@@ -1,52 +1,15 @@
 package main
 
-type position struct {
-	rows int
-	cols int
-}
-
-func nextPos(p position, n int) position {
-	p2 := position{
-		rows: p.rows * 2,
-		cols: p.cols * 2,
-	}
-
-	mid := position{
-		rows: n - 1,
-		cols: n - 1,
-	}
-	v := position{
-		rows: p2.rows - mid.rows,
-		cols: p2.cols - mid.cols,
-	}
-	vo := position{
-		rows: v.cols,
-		cols: -v.rows,
-	}
-	return position{
-		rows: (mid.rows + vo.rows) / 2,
-		cols: (mid.cols + vo.cols) / 2,
-	}
-}
-
-func rotate(matrix [][]int) {
-	n := len(matrix[0])
-	mid := n / 2
-	colMid := (n + 1) / 2
-	for row := 0; row < mid; row++ {
-		for col := 0; col < colMid; col++ {
-			prev := matrix[row][col]
-			p := position{
-				rows: row,
-				cols: col,
-			}
-			for i := 0; i < 3; i++ {
-				p = nextPos(p, n)
-				tmp := matrix[p.rows][p.cols]
-				matrix[p.rows][p.cols] = prev
-				prev = tmp
-			}
-			matrix[row][col] = prev
+func removeElement(nums []int, val int) int {
+	last := len(nums)
+	for i := 0; i < last; {
+		if nums[i] != val {
+			i++
+			continue
 		}
+
+		last--
+		nums[i] = nums[last]
 	}
+	return last
 }
--- main_test.go
+++ main_test.go
@@ -6,56 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNextPos(t *testing.T) {
-	assert.Equal(t,
-		position{rows: 0, cols: 2},
-		nextPos(position{rows: 0, cols: 0}, 3),
-	)
-
-	assert.Equal(t,
-		position{rows: 2, cols: 2},
-		nextPos(position{rows: 0, cols: 2}, 3),
-	)
-
-	assert.Equal(t,
-		position{rows: 2, cols: 0},
-		nextPos(position{rows: 2, cols: 2}, 3),
-	)
-}
-
-func TestNextPos_Bigger_Space(t *testing.T) {
-	assert.Equal(t,
-		position{rows: 2, cols: 2},
-		nextPos(position{rows: 1, cols: 2}, 4),
-	)
-}
-
-func TestSample(t *testing.T) {
-	m := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
-	rotate(m)
-	assert.Equal(t, [][]int{
-		{7, 4, 1},
-		{8, 5, 2},
-		{9, 6, 3},
-	}, m)
-}
-
-func TestSample_Bigger(t *testing.T) {
-	m := [][]int{
-		{5, 1, 9, 11},
-		{2, 4, 8, 10},
-		{13, 3, 6, 7},
-		{15, 14, 12, 16},
-	}
-	rotate(m)
-	assert.Equal(t, [][]int{
-		{15, 13, 2, 5},
-		{14, 3, 4, 1},
-		{12, 6, 8, 9},
-		{16, 7, 10, 11},
-	}, m)
+func TestRemove(t *testing.T) {
+	a := []int{3, 2, 2, 3}
+	index := removeElement(a, 3)
+	assert.Equal(t, []int{2, 2}, a[:index])
 }
