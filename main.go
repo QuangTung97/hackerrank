d7e1f0eab57de05a9334cb2da060cebee9cf39db
tung.tq <tung.tq@teko.vn>
2024-03-06 15:42:18 +0700

    53. Maximum Subarray - Medium

--- main.go
+++ main.go
@@ -1,67 +1,16 @@
 package main
 
-import (
-	"math"
-)
-
-func computeInit(nums []int) []int {
-	n := len(nums)
-	size := 0
-	for n > 1 {
-		size += n
-		n = (n + 1) / 2
-	}
-	size++
-	nodes := make([]int, size)
-
-	copy(nodes[size-len(nums):], nums)
-	return nodes
-}
-
-func coverRange(i int, n int) int {
-	return 0
-}
-
-func buildTree(nums []int) []int {
-	nodes := computeInit(nums)
-
-	n := len(nums)
-	start := len(nodes) - n
-	for {
-		n = (n + 1) / 2
-		oldStart := start
-		start -= n
-
-		for i := 0; i < n; i++ {
-			l := oldStart + 2*i
-			r := oldStart + 2*i + 1
-			nodes[start+i] = min(nodes[l], nodes[r])
+func maxSubArray(nums []int) int {
+	res := nums[0]
+	for start := 0; start < len(nums); start++ {
+		res = max(res, nums[start])
+		if nums[start] <= 0 {
+			continue
 		}
-		if start == 0 {
-			break
+		if start+1 >= len(nums) {
+			continue
 		}
+		nums[start+1] += nums[start]
 	}
-
-	return nodes
-}
-
-func treeMinOf(nodes []int, end int) int {
-	return 0
-}
-
-func jump(nums []int) int {
-	minArr := make([]int, len(nums))
-	for i := range minArr {
-		minArr[i] = math.MaxInt32
-	}
-	minArr[0] = 0
-	for i := 1; i < len(nums); i++ {
-		for j := 0; j < i; j++ {
-			step := nums[j]
-			if j+step >= i {
-				minArr[i] = min(minArr[i], minArr[j]+1)
-			}
-		}
-	}
-	return minArr[len(minArr)-1]
+	return res
 }
--- main_test.go
+++ main_test.go
@@ -6,18 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCoverRange(t *testing.T) {
-	assert.Equal(t, 5, coverRange(0, 5))
-	assert.Equal(t, 5, coverRange(1, 5))
+func TestMaxSubArr(t *testing.T) {
+	assert.Equal(t, 6, maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	assert.Equal(t, 1, maxSubArray([]int{1}))
 }
 
-func TestBuildSegmentTree(t *testing.T) {
-	nodes := buildTree([]int{5, 4, 8, 3})
-	assert.Equal(t, []int{3, 4, 3, 5, 4, 8, 3}, nodes)
-
-	assert.Equal(t, 4, treeMinOf(nodes, 0))
-}
-
-func TestJumpGame(t *testing.T) {
-	assert.Equal(t, 2, jump([]int{2, 3, 1, 1, 4}))
+func TestExample1(t *testing.T) {
+	arr := []int{1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4}
+	assert.Equal(t, 6, maxSubArray(arr))
 }
