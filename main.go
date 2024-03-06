38335cf477dd6bd4bdbdb23f5065e3d45f35f34b
tung.tq <tung.tq@teko.vn>
2024-03-06 15:49:36 +0700

    Improve Number of Lines

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 func maxSubArray(nums []int) int {
 	res := nums[0]
+	prefixSum := 0
 	for start := 0; start < len(nums); start++ {
-		res = max(res, nums[start])
-		if nums[start] <= 0 {
+		prefixSum += nums[start]
+		res = max(res, prefixSum)
+		if prefixSum <= 0 {
+			prefixSum = 0
 			continue
 		}
-		if start+1 >= len(nums) {
-			continue
-		}
-		nums[start+1] += nums[start]
 	}
 	return res
 }
