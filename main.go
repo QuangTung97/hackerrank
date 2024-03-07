cc1128d9b8a73653d457c41bc9bac3c31a783e61
tung.tq <tung.tq@teko.vn>
2024-03-07 09:09:44 +0700

    49. Group Anagrams - Medium

--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
-func maxSubArray(nums []int) int {
-	res := nums[0]
-	prefixSum := 0
-	for start := 0; start < len(nums); start++ {
-		prefixSum += nums[start]
-		res = max(res, prefixSum)
-		if prefixSum <= 0 {
-			prefixSum = 0
+import (
+	"slices"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	type position struct {
+		key   string
+		index int
+	}
+
+	posList := make([]position, 0, len(strs))
+	for index, s := range strs {
+		key := []byte(s)
+		slices.Sort(key)
+		posList = append(posList, position{
+			key:   string(key),
+			index: index,
+		})
+	}
+
+	slices.SortFunc(posList, func(a, b position) int {
+		if a.key < b.key {
+			return -1
+		}
+		if a.key == b.key {
+			return 0
+		}
+		return 1
+	})
+
+	result := make([][]string, 0, len(strs)/2)
+	result = append(result, []string{
+		strs[posList[0].index],
+	})
+
+	lastKey := posList[0].key
+	for _, pos := range posList[1:] {
+		if pos.key == lastKey {
+			n := len(result)
+			result[n-1] = append(result[n-1], strs[pos.index])
 			continue
 		}
+		lastKey = pos.key
+		result = append(result, []string{
+			strs[pos.index],
+		})
 	}
-	return res
+
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestMaxSubArr(t *testing.T) {
-	assert.Equal(t, 6, maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
-	assert.Equal(t, 1, maxSubArray([]int{1}))
-}
-
-func TestExample1(t *testing.T) {
-	arr := []int{1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4}
-	assert.Equal(t, 6, maxSubArray(arr))
+	assert.Equal(t, [][]string{
+		{"bat"},
+		{"ate", "eat", "tea"},
+		{"nat", "tan"},
+	}, groupAnagrams([]string{
+		"eat", "tea", "tan", "ate", "nat", "bat",
+	}))
 }
