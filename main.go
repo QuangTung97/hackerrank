13a8f69e0dc7c6ae2c9bc1939ab388c8cc3e4467
tung.tq <tung.tq@teko.vn>
2024-03-08 10:10:04 +0700

    36. Valid Sudoku - Medium

--- main.go
+++ main.go
@@ -1,76 +1,46 @@
 package main
 
-import (
-	"slices"
-)
-
-type state struct {
-	visited []bool
-	output  [][][]int
-	existed map[string]struct{}
-}
-
-func newState() *state {
-	return &state{
-		visited: make([]bool, 41),
-		output:  make([][][]int, 41),
-		existed: map[string]struct{}{},
-	}
-}
-
-func numsToKey(nums []int) string {
-	res := make([]byte, 0, len(nums))
-	for _, n := range nums {
-		res = append(res, byte(n)+'A')
-	}
-	slices.Sort(res)
-	return string(res)
-}
-
-func (s *state) compute(candidates []int, target int) [][]int {
-	if s.visited[target] {
-		return s.output[target]
-	}
-
-	s.visited[target] = true
-
-	var l [][]int
-
-	for _, e := range candidates {
-		if e > target {
-			break
-		}
-
-		if target == e {
-			l = append(l, []int{
-				e,
-			})
-			continue
-		}
-
-		newTarget := target - e
-		subLists := s.compute(candidates, newTarget)
-		for _, subList := range subLists {
-			res := make([]int, 0, len(subList)+1)
-			res = append(res, e)
-			res = append(res, subList...)
-
-			key := numsToKey(res)
-			_, ok := s.existed[key]
-			if ok {
+func checkRegion(
+	board [][]byte,
+	rowBegin int, rowEnd int,
+	colBegin int, colEnd int,
+) bool {
+	set := map[byte]struct{}{}
+	for row := rowBegin; row < rowEnd; row++ {
+		for col := colBegin; col < colEnd; col++ {
+			e := board[row][col]
+			if e == '.' {
 				continue
 			}
-			s.existed[key] = struct{}{}
-			l = append(l, res)
+			num := e - '0'
+			_, existed := set[num]
+			if existed {
+				return false
+			}
+			set[num] = struct{}{}
+		}
+	}
+	return true
+}
+
+func isValidSudoku(board [][]byte) bool {
+	for row := 0; row < 9; row++ {
+		if ok := checkRegion(board, row, row+1, 0, 9); !ok {
+			return false
+		}
+	}
+	for col := 0; col < 9; col++ {
+		if ok := checkRegion(board, 0, 9, col, col+1); !ok {
+			return false
+		}
+	}
+	for row := 0; row < 9; row += 3 {
+		for col := 0; col < 9; col += 3 {
+			if ok := checkRegion(board, row, row+3, col, col+3); !ok {
+				return false
+			}
 		}
 	}
 
-	s.output[target] = l
-	return s.output[target]
-}
-
-func combinationSum(candidates []int, target int) [][]int {
-	slices.Sort(candidates)
-	s := newState()
-	return s.compute(candidates, target)
+	return true
 }
