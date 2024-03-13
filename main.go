5a6175a5b627a86a69ae0dbe91f3ee35026fa3a0
tung.tq <tung.tq@teko.vn>
2024-03-13 09:55:25 +0700

    77. Combinations - Medium

--- main.go
+++ main.go
@@ -1,30 +1,41 @@
 package main
 
-func minDistance(word1 string, word2 string) int {
-	oldTable := make([]int, len(word1)+1)
-	newTable := make([]int, len(word1)+1)
-	for i := range oldTable {
-		oldTable[i] = i
-	}
+import (
+	"slices"
+)
 
-	for j := 1; j <= len(word2); j++ {
-		for i := 0; i <= len(word1); i++ {
-			if i == 0 {
-				newTable[i] = j
-				continue
-			}
-			ch1 := word1[i-1]
-			ch2 := word2[j-1]
-			if ch1 == ch2 {
-				newTable[i] = oldTable[i-1]
-				continue
-			}
-
-			newTable[i] = min(newTable[i-1], oldTable[i-1], oldTable[i]) + 1
+func nextIter(it []int, n int) bool {
+	for i := len(it) - 1; i >= 0; i-- {
+		right := len(it) - 1 - i
+		if it[i] >= n-right {
+			continue
 		}
-		tmp := oldTable
-		oldTable = newTable
-		newTable = tmp
+
+		next := it[i]
+		for ; i < len(it); i++ {
+			next++
+			it[i] = next
+		}
+		return true
 	}
-	return oldTable[len(word1)]
+	return false
+}
+
+func combine(n int, k int) [][]int {
+	var result [][]int
+
+	iter := make([]int, k)
+	for i := range iter {
+		iter[i] = i + 1
+	}
+
+	for {
+		result = append(result, slices.Clone(iter))
+		ok := nextIter(iter, n)
+		if !ok {
+			break
+		}
+	}
+
+	return result
 }
--- main_test.go
+++ main_test.go
@@ -6,24 +6,58 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSetZeros(t *testing.T) {
-	t.Run("simple", func(t *testing.T) {
-		assert.Equal(t, 1, minDistance("a", ""))
-	})
+func TestNextIter(t *testing.T) {
+	iter := []int{1, 2, 3}
 
-	t.Run("more complex", func(t *testing.T) {
-		assert.Equal(t, 2, minDistance("aa", ""))
-	})
+	nextIter(iter, 4)
+	assert.Equal(t, []int{1, 2, 4}, iter)
 
-	t.Run("more complex", func(t *testing.T) {
-		assert.Equal(t, 1, minDistance("aa", "ab"))
-	})
+	nextIter(iter, 4)
+	assert.Equal(t, []int{1, 3, 4}, iter)
 
-	t.Run("ex1", func(t *testing.T) {
-		assert.Equal(t, 3, minDistance("horse", "ros"))
-	})
+	nextIter(iter, 4)
+	assert.Equal(t, []int{2, 3, 4}, iter)
+}
 
-	t.Run("ex2", func(t *testing.T) {
-		assert.Equal(t, 5, minDistance("intention", "execution"))
-	})
+func TestNextEx2(t *testing.T) {
+	var ok bool
+	iter := []int{1, 2}
+
+	nextIter(iter, 4)
+	assert.Equal(t, []int{1, 3}, iter)
+
+	nextIter(iter, 4)
+	assert.Equal(t, []int{1, 4}, iter)
+
+	ok = nextIter(iter, 4)
+	assert.Equal(t, []int{2, 3}, iter)
+	assert.Equal(t, true, ok)
+
+	nextIter(iter, 4)
+	assert.Equal(t, []int{2, 4}, iter)
+
+	ok = nextIter(iter, 4)
+	assert.Equal(t, []int{3, 4}, iter)
+	assert.Equal(t, true, ok)
+
+	ok = nextIter(iter, 4)
+	assert.Equal(t, []int{3, 4}, iter)
+	assert.Equal(t, false, ok)
+}
+
+func TestCombine(t *testing.T) {
+	assert.Equal(t, [][]int{
+		{1, 2}, {1, 3}, {1, 4},
+		{2, 3}, {2, 4},
+		{3, 4},
+	}, combine(4, 2))
+}
+
+func TestCombine_Ex2(t *testing.T) {
+	assert.Equal(t, [][]int{
+		{1, 2, 3},
+		{1, 2, 4},
+		{1, 3, 4},
+		{2, 3, 4},
+	}, combine(4, 3))
 }
