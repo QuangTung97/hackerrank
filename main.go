569f6453694f55b98e0dda47b863c03e9c25f7c3
tung.tq <tung.tq@teko.vn>
2024-03-14 08:58:21 +0700

    80. Remove Duplicates from Sorted Array II - Medium

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
 
-import (
-	"math/bits"
-)
-
-func subsets(nums []int) [][]int {
-	n := len(nums)
-	maxVal := uint16(1 << n)
-	result := make([][]int, 0, maxVal)
-	for i := uint16(0); i < maxVal; i++ {
-		value := make([]int, 0, bits.OnesCount16(i))
-		mask := uint16(1)
-		for offset := uint16(0); offset < uint16(n); offset++ {
-			if i&mask != 0 {
-				value = append(value, nums[offset])
+func removeDuplicates(nums []int) int {
+	left := 1
+	duplicateCount := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[left-1] {
+			duplicateCount++
+			if duplicateCount >= 2 {
+				duplicateCount = 1
+				continue
 			}
-			mask <<= 1
+		} else {
+			duplicateCount = 0
 		}
 
-		result = append(result, value)
+		nums[left] = nums[i]
+		left++
 	}
-	return result
+
+	return left
 }
--- main_test.go
+++ main_test.go
@@ -6,10 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSubset(t *testing.T) {
-	assert.Equal(t, [][]int{
-		{},
-		{2}, {3},
-		{2, 3},
-	}, subsets([]int{2, 3}))
+func TestRemoveDuplicate(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		nums := []int{1, 1, 1, 2, 2, 3}
+		n := removeDuplicates(nums)
+		assert.Equal(t, []int{1, 1, 2, 2, 3}, nums[:n])
+	})
+
+	t.Run("normal", func(t *testing.T) {
+		nums := []int{0, 0, 1, 1, 1, 1, 2, 3, 3}
+		n := removeDuplicates(nums)
+		assert.Equal(t, []int{0, 0, 1, 1, 2, 3, 3}, nums[:n])
+	})
+
+	t.Run("ex1", func(t *testing.T) {
+		nums := []int{1, 2, 2}
+		n := removeDuplicates(nums)
+		assert.Equal(t, []int{1, 2, 2}, nums[:n])
+	})
+
+	t.Run("ex2", func(t *testing.T) {
+		nums := []int{0, 0, 1, 1, 1, 1, 2, 3, 3}
+		n := removeDuplicates(nums)
+		assert.Equal(t, []int{0, 0, 1, 1, 2, 3, 3}, nums[:n])
+	})
 }
